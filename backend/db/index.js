442568a998cafda3d034634aea1b7d2ea75cbcf1
Nitto Thomas <160035598+nittothomas94@users.noreply.github.com>
2025-02-25 21:03:17 +0530

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose
-  .connect('mongodb+srv://nittothomas94:Nitto123@cluster0.guv3w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://nittothomas94:Nitto123@cluster0.guv3w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' 
+
+mongoose 
+  .connect(mongoURI)
   .then(() => {
     console.log('DB Connected');
   })
   .catch(e => {
-    console.log(e);
+    console.log('DB Connection Error',e);
   });
 
 module.exports = mongoose;
