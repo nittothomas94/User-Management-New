e17bb4aa3d05c95f6d3942018ff0fbfd405dcea7
Nitto Thomas <160035598+nittothomas94@users.noreply.github.com>
2025-02-25 21:15:38 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://user-management-new-backend.onrender.com/api',
   timeout: 5000,
 });
 
