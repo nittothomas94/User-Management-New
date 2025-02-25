2eab44895546a25ac833eab995773432d3b07ee8
Nitto Thomas <160035598+nittothomas94@users.noreply.github.com>
2025-02-25 19:53:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 
+const app = express();
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
@@ -16,6 +17,6 @@ app.use('*', (req, res) => {
   return res.status(404).json({ message: 'Page Not Found' });
 });
 
-app.listen(3000, () => {
-  console.log('App is Runnig at http://localhost/3000');
+app.listen(port, () => {
+  console.log('App is Runnig');
 });
