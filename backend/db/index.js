54834b4260f0adefbef6830849223f3d6645a2e9
Nitto Thomas <160035598+nittothomas94@users.noreply.github.com>
2025-02-25 20:27:52 +0530

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost:27017/user-Management')
+  .connect('mongodb://nittothomas94:Nitto123@cluster0-shard-00-00.guv3w.mongodb.net:27017,cluster0-shard-00-01.guv3w.mongodb.net:27017,cluster0-shard-00-02.guv3w.mongodb.net:27017/?replicaSet=atlas-at15e4-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster0')
   .then(() => {
     console.log('DB Connected');
   })
